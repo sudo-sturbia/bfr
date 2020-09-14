62b561ab0110ba030684a9d7b9a1672e46d792a4
Zyad A. Ali <zyad.ali.me@gmail.com>
2020-09-14 12:31:06 +0200

    books: Create test cases for SearchForTitles.

--- pkg/books/books_test.go
+++ pkg/books/books_test.go
@@ -261,6 +261,127 @@ func TestSearch(t *testing.T) {
 	}
 }
 
+// Test searching for books' titles using SearchBy.
+func TestSearchForTitles(t *testing.T) {
+	config := &datastore.Config{
+		Driver:    "sqlite3",
+		Datastore: "testDatastore.db",
+		BookTable: "books",
+	}
+
+	err := datastore.New("../../test-data/booksTest.csv", config, true)
+	if err != nil {
+		t.Errorf("Couldn't load datastore: %s.", err.Error())
+		return
+	}
+
+	datastore, err := sql.Open(config.Driver, fmt.Sprintf("file:%s", config.Datastore))
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	defer datastore.Close()
+	defer os.Remove(config.Datastore)
+
+	searchIn := &SearchIn{
+		Datastore: datastore,
+		BookTable: config.BookTable,
+	}
+
+	titles := map[*SearchBy][]string{
+		&SearchBy{
+			TitleHas:          "Secrets",
+			Authors:           nil,
+			LanguageCode:      nil,
+			ISBN:              "",
+			ISBN13:            "",
+			RatingCeil:        -1,
+			RatingFloor:       -1,
+			PagesCeil:         -1,
+			PagesFloor:        -1,
+			RatingsCountCeil:  -1,
+			RatingsCountFloor: -1,
+			ReviewsCountCeil:  -1,
+			ReviewsCountFloor: -1,
+		}: []string{
+			"Harry Potter and the Chamber of Secrets (Harry Potter  #2)",
+		},
+
+		&SearchBy{
+			TitleHas:          "",
+			Authors:           nil,
+			LanguageCode:      nil,
+			ISBN:              "",
+			ISBN13:            "",
+			RatingCeil:        -1,
+			RatingFloor:       4.7,
+			PagesCeil:         -1,
+			PagesFloor:        -1,
+			RatingsCountCeil:  -1,
+			RatingsCountFloor: -1,
+			ReviewsCountCeil:  -1,
+			ReviewsCountFloor: -1,
+		}: []string{
+			"Harry Potter Boxed Set  Books 1-5 (Harry Potter  #1-5)",
+			"Harry Potter Collection (Harry Potter  #1-6)",
+		},
+
+		&SearchBy{
+			TitleHas:          "",
+			Authors:           []string{"Bill", "Adams"},
+			LanguageCode:      nil,
+			ISBN:              "",
+			ISBN13:            "",
+			RatingCeil:        -1,
+			RatingFloor:       -1,
+			PagesCeil:         400,
+			PagesFloor:        200,
+			RatingsCountCeil:  -1,
+			RatingsCountFloor: -1,
+			ReviewsCountCeil:  -1,
+			ReviewsCountFloor: 2000,
+		}: []string{
+			"In a Sunburned Country",
+			"I'm a Stranger Here Myself: Notes on Returning to America After Twenty Years Away",
+		},
+
+		&SearchBy{
+			TitleHas:          "",
+			Authors:           nil,
+			LanguageCode:      []string{"fre"},
+			ISBN:              "",
+			ISBN13:            "",
+			RatingCeil:        -1,
+			RatingFloor:       -1,
+			PagesCeil:         -1,
+			PagesFloor:        -1,
+			RatingsCountCeil:  -1,
+			RatingsCountFloor: -1,
+			ReviewsCountCeil:  -1,
+			ReviewsCountFloor: -1,
+		}: []string{},
+	}
+
+	for searchBy, title := range titles {
+		result, err := SearchForTitles(searchIn, searchBy)
+		if err != nil {
+			t.Errorf("Search failed.")
+			continue
+		}
+
+		if len(result) != len(title) {
+			t.Errorf("Expected %d search result, Found %d.", 1, len(result))
+			continue
+		}
+
+		for i, res := range result {
+			if res != title[i] {
+				t.Errorf("Incorrect search result for %s.", title[i])
+			}
+		}
+	}
+}
+
 // compareSlices returns true if a and b are equal, false otherwise.
 func compareSlices(a, b []interface{}) bool {
 	if len(a) != len(b) {
