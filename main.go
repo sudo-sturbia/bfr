f4227365f875ca2cfbca1299658bc4f323cdeb0a
Zyad A. Ali <zyad.ali.me@gmail.com>
2020-12-20 17:35:37 +0200

    main: re-arrange main package
    Re-arrange main function and remove unnecessary functions.

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
-// Package main runs a new server instance.
+// Package main runs a server instance.
 package main
 
 import (
 	"flag"
 	"fmt"
-	"os"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/sudo-sturbia/bfr/internal/config"
@@ -13,68 +12,51 @@ import (
 )
 
 var (
-	cfg = config.New()
+	port    = flag.String("port", "", "Specify a port to run the server on.")
+	dataset = flag.String("dataset", "", "Load a new csv dataset from specified path.")
 )
 
 func main() {
-	parseFlags()
+	flag.Usage = usage
+	flag.Parse()
+
+	cfg := config.New()
+
+	if *port != "" {
+		cfg.Server.Port = *port
+	}
+	if *dataset != "" {
+		err := datastore.New(*dataset, cfg.Datastore, true)
+		if err != nil {
+			log.Fatalf("Failed to create a datastore: %s.", err.Error())
+		}
+	}
 
 	datastore, err := datastore.Open(cfg.Datastore)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	server := server.New(cfg.Server, &server.SearchIn{
-		Datastore: datastore,
-		BookTable: cfg.Datastore.BookTable,
-	})
+	server := server.New(
+		cfg.Server, &server.SearchIn{
+			Datastore: datastore,
+			BookTable: cfg.Datastore.BookTable,
+		},
+	)
 
 	server.Run()
 }
 
-// parseFlags parses, and handles command line flags.
-func parseFlags() {
-	help := flag.Bool("h", false, "Print a help message.")
-	port := flag.String("port", "", "Specify a port to run the server on.")
-	dataset := flag.String("dataset", "", "Load a new csv dataset from specified path.")
-	flag.Parse()
-
-	if *help {
-		description()
-	}
-	if *port != "" {
-		specifyPort(*port)
-	}
-	if *dataset != "" {
-		createDatastore(*dataset)
-	}
-}
-
-// description prints a help message.
-func description() {
-	defer os.Exit(0)
+// usage prints a help message.
+func usage() {
 	fmt.Printf(
 		"%s\n%s\n%s\n%s\n%s\n%s\n\n%s\n",
-		"bfr is a REST API to search for books using a set of parameters.",
+		"bfr is a web server that enables searching for books using a set of parameters.",
 		"Usage:",
-		"    bfr                 Runs as a web server at :6060.",
-		"    bfr -dataset path   Loads a new csv dataset to use as a datastore, then runs the server.",
-		"    bfr -port <number>  Specifies a different port to run the server on.",
-		"    bfr -h              Prints a help message.",
+		"    bfr                 Run as a web server at :6060.",
+		"    bfr -dataset path   Load a new csv dataset to use as a datastore, then runs the server.",
+		"    bfr -port <number>  Use the specified port to run the server.",
+		"    bfr -h              Print a help message.",
 		"See github.com/sudo-sturbia/bfr.",
 	)
-}
-
-// specifyPort updates the port that the server runs on.
-func specifyPort(port string) {
-	cfg.Server.Port = port
-}
-
-// createDatastore creates a new datastore using dataset at specified
-// path. Datastore is created at position specified by Config.
-func createDatastore(dataset string) {
-	err := datastore.New(dataset, cfg.Datastore, true)
-	if err != nil {
-		log.Fatalf("Failed to create a datastore: %s.", err.Error())
-	}
 }
