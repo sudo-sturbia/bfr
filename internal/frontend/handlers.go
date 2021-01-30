506e6bea6cdb3770ba33e0d2858268791e132285
Zyad A. Ali <zyad.ali.me@gmail.com>
2021-01-30 16:25:15 +0200

    internal/frontend: create initial server
    Implement initial frontend server with empty handlers.
    
    For #1

---
+++ internal/frontend/handlers.go
@@ -0,0 +1,14 @@
+package frontend
+
+import (
+	"net/http"
+)
+
+func (s *Server) searchForm(w http.ResponseWriter, r *http.Request) {
+}
+
+func (s *Server) searchResults(w http.ResponseWriter, r *http.Request) {
+}
+
+func (s *Server) serveBook(w http.ResponseWriter, r *http.Request) {
+}
